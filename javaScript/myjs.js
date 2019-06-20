d0bdde8052dd7ebae0671a5c6b445ecab0a14192
Rahul Pathak <rahulzha93@gmail.com>
2019-06-20 18:33:42 +0545

    js

---
+++ javaScript/myjs.js
@@ -0,0 +1,5 @@
+ // document.write("my first JavaScript");
+        // window.alert("My first JavScript");
+        // console.log("My first javaScript");
+        // document.getElementById('p1').innerHTML = "My first JavaScript";
+        // document.getElementById('p2').innerHTML = "i am a head tag";
